4b39e1552f7a4ad1c22604d6b8fc0dcf4bb091e2
a_pankratyev <a_pankratyev@wargaming.net>
2017-08-14 00:13:15 +0300

    Added secret for db

--- k8s-helm-exampleapp/image_base_app/bin/uwsgi.sh
+++ k8s-helm-exampleapp/image_base_app/bin/uwsgi.sh
@@ -2,4 +2,5 @@
 echo "=======Run $0 for exampleapp `pwd`========"
 
 echo "--- Start uwsgi service exampleapp ---"
-su -l httpd -c '/usr/sbin/uwsgi --ini /home/httpd/conf/uwsgi/uwsgi.conf  --logto /home/httpd/logs/httpd_uwsgi_access_service.log'
+
+/usr/sbin/uwsgi --ini /home/httpd/conf/uwsgi/uwsgi.conf  --logto /home/httpd/logs/httpd_uwsgi_access_service.log
